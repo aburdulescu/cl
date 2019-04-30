a35f812237aebf8f35c18ffb74775c9bcfa03842
aburdulescu <andrei.burdulescu@protonmail.com>
2019-04-30 21:19:51 +0300

    Move code around

--- main.go
+++ main.go
@@ -18,6 +18,106 @@ type Color struct {
 	Func func(a ...interface{}) string
 }
 
+func main() {
+	flag.CommandLine.Usage = func() {
+		header := `%s -x regex INPUT
+
+Read INPUT and color the part of the line that matches regex with the
+color specified by -x flag(see Colors).
+
+Examples:
+1) color the lines from file "file.txt" that end with "foo" with color blue:
+    %s -b ".*foo$" file.txt
+
+Colors:
+`
+		fmt.Fprintf(flag.CommandLine.Output(), header, os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	type Flag struct {
+		pattern   string
+		colorAttr color.Attribute
+	}
+
+	flags := map[string]*Flag{
+		"blue":    &Flag{colorAttr: color.FgBlue},
+		"cyan":    &Flag{colorAttr: color.FgCyan},
+		"green":   &Flag{colorAttr: color.FgGreen},
+		"magenta": &Flag{colorAttr: color.FgMagenta},
+		"red":     &Flag{colorAttr: color.FgRed},
+		"yellow":  &Flag{colorAttr: color.FgYellow},
+	}
+	flag.StringVar(&flags["blue"].pattern, "b", "", "color blue")
+	flag.StringVar(&flags["cyan"].pattern, "c", "", "color cyan")
+	flag.StringVar(&flags["green"].pattern, "g", "", "color green")
+	flag.StringVar(&flags["magenta"].pattern, "m", "", "color magenta")
+	flag.StringVar(&flags["red"].pattern, "r", "", "color red")
+	flag.StringVar(&flags["yellow"].pattern, "y", "", "color yellow")
+
+	var cpuprof string
+	flag.StringVar(&cpuprof, "cpuprof", "", "write cpu profile to file")
+	flag.Parse()
+
+	if cpuprof != "" {
+		f, err := os.Create(cpuprof)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if len(os.Args) == 1 {
+		mainError(fmt.Errorf("no flags provided"))
+	}
+
+	var colors []Color
+	for _, v := range flags {
+		if v.pattern == "" {
+			continue
+		}
+		re, err := regexp.Compile(v.pattern)
+		if err != nil {
+			mainError(err)
+		}
+		color := Color{Re: re, Func: color.New(v.colorAttr).SprintFunc()}
+		colors = append(colors, color)
+	}
+
+	var scanner *bufio.Scanner
+	if len(flag.Args()) == 0 {
+		scanner = bufio.NewScanner(bufio.NewReader(os.Stdin))
+	} else {
+		file, err := os.Open(flag.Arg(0))
+		if err != nil {
+			mainError(err)
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(bufio.NewReader(file))
+	}
+
+	color.NoColor = false // always output color
+	w := bufio.NewWriter(os.Stdout)
+	for scanner.Scan() {
+		line := colorLine(colors, scanner.Text())
+		var output strings.Builder
+		fmt.Fprintf(&output, "%s\n", line)
+		if _, err := w.WriteString(output.String()); err != nil {
+			mainError(err)
+		}
+	}
+	w.Flush()
+	if err := scanner.Err(); err != nil {
+		mainError(err)
+	}
+}
+
+func mainError(err error) {
+	fmt.Fprintln(os.Stderr, "error:", err)
+	os.Exit(1)
+}
+
 func contains(idxs [][]int, i int) bool {
 	for _, idx := range idxs {
 		if i >= idx[0] && i < idx[1] {
@@ -70,104 +170,4 @@ func colorLine(colors []Color, line string) string {
 	}
 
 	return output.String()
-}
-
-func main() {
-	flag.CommandLine.Usage = func() {
-		header := `%s -x regex INPUT
-
-Read INPUT and color the part of the line that matches regex with the
-color specified by -x flag(see Colors).
-
-Examples:
-1) color the lines from file "file.txt" that end with "foo" with color blue:
-    %s -b ".*foo$" file.txt
-
-Colors:
-`
-		fmt.Fprintf(flag.CommandLine.Output(), header, os.Args[0], os.Args[0])
-		flag.PrintDefaults()
-	}
-
-	type Flag struct {
-		pattern   string
-		colorAttr color.Attribute
-	}
-
-	flags := map[string]*Flag{
-		"blue":    &Flag{colorAttr: color.FgBlue},
-		"cyan":    &Flag{colorAttr: color.FgCyan},
-		"green":   &Flag{colorAttr: color.FgGreen},
-		"magenta": &Flag{colorAttr: color.FgMagenta},
-		"red":     &Flag{colorAttr: color.FgRed},
-		"yellow":  &Flag{colorAttr: color.FgYellow},
-	}
-	flag.StringVar(&flags["blue"].pattern, "b", "", "color blue")
-	flag.StringVar(&flags["cyan"].pattern, "c", "", "color cyan")
-	flag.StringVar(&flags["green"].pattern, "g", "", "color green")
-	flag.StringVar(&flags["magenta"].pattern, "m", "", "color magenta")
-	flag.StringVar(&flags["red"].pattern, "r", "", "color red")
-	flag.StringVar(&flags["yellow"].pattern, "y", "", "color yellow")
-
-	var cpuprof string
-	flag.StringVar(&cpuprof, "cpuprof", "", "write cpu profile to file")
-	flag.Parse()
-
-	if cpuprof != "" {
-		f, err := os.Create(cpuprof)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
-
-	if len(os.Args) == 1 {
-		fmt.Fprintf(os.Stderr, "error: no flags provided\n")
-		os.Exit(1)
-	}
-
-	var colors []Color
-	for _, v := range flags {
-		if v.pattern == "" {
-			continue
-		}
-		re, err := regexp.Compile(v.pattern)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s\n", err)
-			os.Exit(1)
-		}
-		color := Color{Re: re, Func: color.New(v.colorAttr).SprintFunc()}
-		colors = append(colors, color)
-	}
-
-	var scanner *bufio.Scanner
-	if len(flag.Args()) == 0 {
-		scanner = bufio.NewScanner(bufio.NewReader(os.Stdin))
-	} else {
-		file, err := os.Open(flag.Arg(0))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s\n", err)
-			os.Exit(1)
-		}
-		defer file.Close()
-		scanner = bufio.NewScanner(bufio.NewReader(file))
-	}
-
-	color.NoColor = false // always output color
-	w := bufio.NewWriter(os.Stdout)
-	for scanner.Scan() {
-		line := colorLine(colors, scanner.Text())
-		var output strings.Builder
-		fmt.Fprintf(&output, "%s\n", line)
-		if _, err := w.WriteString(output.String()); err != nil {
-			fmt.Fprintln(os.Stderr, "error: ", err)
-			os.Exit(1)
-		}
-	}
-	w.Flush()
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error: ", err)
-		os.Exit(1)
-	}
 }
